a1ab23926299483ad650e82f58cbc89369b15e57
Rawan Aladdin <raladdin@my.bcit.ca>
2024-10-23 16:46:48 -0700

    started on dashboard, fixed sidenav, fixed topnav, added first card, starting file for onboarding, downloaded joyride for onboarding, adjusting home css file for padding

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import "@/styles/theme"
+import "@/styles/theme";
 import "@phosphor-icons/web/light";
 import "@phosphor-icons/web/bold";
-import { Text } from "@chakra-ui/react";
+import { Heading, Box, CardBody, Text, Stack, Card } from "@chakra-ui/react";
 import { Button as ChakraButton } from '@chakra-ui/react';
 import TopNav from "@/styles/components/topnav";
 import { SideNavBar } from "@/styles/components/sidenav";
@@ -20,8 +20,40 @@ export default function Home() {
       </Head>
       <div className={`${styles.page}`}>
         <TopNav />
-        <main className={styles.main}>
-          <SideNavBar />
+        <main className={styles.main} style={{ position: "relative" }}>
+          <SideNavBar property1="default" />
+          <Box className="content" m={8}>
+            <Heading
+              fontFamily="heading"
+              color="black" 
+              fontWeight="bold" 
+              fontSize="lg"
+              zIndex={10}
+              position="relative"
+            >
+              <span style={{ color: "#EA4A7D"}}>
+                Welcome back,
+              </span>
+              Onwarder!
+            </Heading>
+            <Card width="35em" mt={7} borderRadius="15px" boxShadow="md">
+              <CardBody>
+                <Stack spacing={4} align="center">
+                  <Text fontFamily="heading" fontSize="sm" fontWeight="bold" textAlign="center">
+                    Ready to take the next step?
+                  </Text>
+                  <Text fontFamily="body" textAlign="center">
+                    Whether you're here preparing.........................
+                  </Text>
+                  <ChakraButton variant="mdPrimary" p={3}>
+                    <Text display="flex" p={2} fontSize="24px">
+                      Start Practicing!
+                    </Text>
+                  </ChakraButton>
+                </Stack>
+              </CardBody>
+            </Card>
+          </Box>
         </main>
         <Footer />
       </div>
