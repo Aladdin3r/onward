1ef1a19936f174d9db5b7a041dda1bc591d38759
Rawan Aladdin <raladdin@my.bcit.ca>
2024-10-28 21:35:26 -0700

    fixed side nav, added layout to mock interview pages

--- pages/mock-interview-start.js
+++ pages/mock-interview-start.js
@@ -5,6 +5,7 @@ import { Box, Flex, Text, Button, IconButton, Stack, Center } from "@chakra-ui/r
 import { Waveform, Microphone, VideoCamera } from "@phosphor-icons/react";
 import VideoPlayer from "@/styles/components/VideoPlayer";
 import { useRouter } from "next/router";
+import Layout from "@/styles/components/Layout";
 
 export default function MockInterviewQuestionPage() {
     const router = useRouter();
@@ -17,77 +18,78 @@ export default function MockInterviewQuestionPage() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Layout>
+                <Box className={styles.page} bg="gray.50" minH="100vh" py={8} display="flex" flexDirection="column" alignItems="center">
+                    
+                    {/* Question Prompt */}
+                    <Center>
+                        <Box bg="white" mt={20} px={6} py={4} borderRadius="md" boxShadow="sm" width="70%">
+                            <Text fontSize="sm" fontWeight="semibold" textAlign="center">
+                                Can you describe a time when you were faced with an emergency situation and had to make a quick decision? How did you prioritize tasks, and what steps did you take to ensure the best possible outcome for the patient?
+                            </Text>
+                        </Box>
+                    </Center>
 
-            <Box className={styles.page} bg="gray.50" minH="100vh" py={8} display="flex" flexDirection="column" alignItems="center">
-                
-                {/* Question Prompt */}
-                <Center mb={6}>
-                    <Box bg="white" mt={20} px={6} py={4} borderRadius="md" boxShadow="sm" width="70%">
-                        <Text fontSize="sm" fontWeight="semibold" textAlign="center">
-                            Can you describe a time when you were faced with an emergency situation and had to make a quick decision? How did you prioritize tasks, and what steps did you take to ensure the best possible outcome for the patient?
-                        </Text>
-                    </Box>
-                </Center>
-
-                {/* Video Player with Audio Overlay Icon */}
-                <Flex justify="center" mb={8} mt={20} position="relative" width="70%">
-                    <VideoPlayer
-                        title="Interview Playback"
-                        thumbnail="/images/smiling-girl.png"
-                    />
-                    {/* Audio Overlay */}
-                    <Box position="absolute" top="4" left="430" bg="blackAlpha.700" p={2} borderRadius="md">
-                        <Waveform size={32} color="white" />
-                    </Box>
-                </Flex>
-
-                {/* Bottom Control Bar */}
-                <Box width="100%" py={8} bg="white" boxShadow="md" position="fixed" bottom={0}>
-                    <Flex justify="space-between" align="center" maxW="800px" mx="auto" px={4}>
-                        {/* End Button */}
-                        <Button colorScheme="red" size="sm" px={8} py={6} 
-                        onClick={() => router.push('/')} >
-                                        {/* "End" will lead to Dashboard for now*/}
-                            End
-                        </Button>
-
-                        {/* Mic and Video Icon Buttons */}
-                        <Stack direction="row" spacing={10}>
-                            <IconButton
-                                px={4}
-                                py={8}
-                                aria-label="Toggle Microphone"
-                                icon={<Microphone size={35} color="black" weight="fill"/>}
-                                bg="white"
-                                boxShadow="md"
-                                borderRadius="full"
-                            />
-                            <IconButton
-                                px={4}
-                                py={8}
-                                aria-label="Toggle Video"
-                                icon={<VideoCamera size={35} color="black" weight="fill"/>}
-                                bg="white"
-                                boxShadow="md"
-                                borderRadius="full"
-                            />
-                        </Stack>
-
-                        {/* Next Question Button */}
-                        <Button
-                            bg="brand.blushPink"
-                            color="brand.frostWhite"
-                            size="sm"
-                            px={8}
-                            py={6}
-                            onClick={() => router.push('/')}
-                                // "Next Question" will lead to Dashboard for now
-                        >
-                            Next Question
-                        </Button>
+                    {/* Video Player with Audio Overlay Icon */}
+                    <Flex justify="center" mb={8} mt={20} position="relative" width="70%">
+                        <VideoPlayer
+                            title="Interview Playback"
+                            thumbnail="/images/smiling-girl.png"
+                        />
+                        {/* Audio Overlay */}
+                        <Box position="absolute" top="4" left="430" bg="blackAlpha.700" p={2} borderRadius="md">
+                            <Waveform size={32} color="white" />
+                        </Box>
                     </Flex>
+
+                    {/* Bottom Control Bar */}
+                    <Box width="100%" py={2} bg="white" boxShadow="md" position="fixed" bottom={0}>
+                        <Flex justify="space-between" align="center" maxW="800px" mx="auto" px={4}>
+                            {/* End Button */}
+                            <Button colorScheme="red" size="sm" px={8} py={6} 
+                            onClick={() => router.push('/')} >
+                                            {/* "End" will lead to Dashboard for now*/}
+                                End
+                            </Button>
+
+                            {/* Mic and Video Icon Buttons */}
+                            <Stack direction="row" spacing={10}>
+                                <IconButton
+                                    px={4}
+                                    py={8}
+                                    aria-label="Toggle Microphone"
+                                    icon={<Microphone size={35} color="black" weight="fill"/>}
+                                    bg="white"
+                                    boxShadow="md"
+                                    borderRadius="full"
+                                />
+                                <IconButton
+                                    px={4}
+                                    py={8}
+                                    aria-label="Toggle Video"
+                                    icon={<VideoCamera size={35} color="black" weight="fill"/>}
+                                    bg="white"
+                                    boxShadow="md"
+                                    borderRadius="full"
+                                />
+                            </Stack>
+
+                            {/* Next Question Button */}
+                            <Button
+                                bg="brand.blushPink"
+                                color="brand.frostWhite"
+                                size="sm"
+                                px={8}
+                                py={6}
+                                onClick={() => router.push('/')}
+                                    // "Next Question" will lead to Dashboard for now
+                            >
+                                Next Question
+                            </Button>
+                        </Flex>
+                    </Box>
                 </Box>
-            </Box>
+            </Layout>
         </>
     );
 }
