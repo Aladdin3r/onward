1e8b5b255cc12e66f339457c483b42a8ea81f3b1
jacksonnsaini <jsaini34@my.bcit.ca>
2024-10-29 00:58:36 -0700

    newDashbaord adjsutments

--- pages/newDashboard.js
+++ pages/newDashboard.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
-import { Box, Heading, Flex } from "@chakra-ui/react";
+import { Box, Heading, Flex, SimpleGrid } from "@chakra-ui/react";
 import Layout from "@/styles/components/Layout";
 import Footer from "@/styles/components/Footer";
 import OverlappingCard from "@/styles/components/DashBoardStartCard";
+import MyResumesCard from "@/styles/components/MyResumesCard";
+import HistoryContainer from "@/styles/components/HistoryContainer";
 
 export default function Home() {
   return (
@@ -15,17 +17,20 @@ export default function Home() {
       </Head>
 
       <Layout showTopNav={false}>
-        <Flex flexDirection="row">
+        <Flex flexDirection="row" justifyContent="center" alignItems="center" width="100%">
           <main style={{ position: "relative", width: "100%" }}>
-            <Flex flexDirection="column">
-              <Box m={6}>
+            <Flex flexDirection="column" alignItems="center">
+              <Box mt={6} mr={300} width="100%" maxWidth="1200px">
                 <Heading fontFamily="heading" color="black" fontWeight="bold" fontSize="32pt">
                   <span style={{ color: "#EA4A7D" }}>Welcome back,</span> Onwarder!
                 </Heading>
-
-                <Box display="flex" justifyContent="space-between" gap={6} mt={7}>
-                  <OverlappingCard />
-                </Box>
+                <SimpleGrid columns={1} spacing={6} mt={7} width="100%">
+                  <Box display="flex" justifyContent="space-between" gap={6}>
+                    <OverlappingCard />
+                    <MyResumesCard />
+                  </Box>
+                  <HistoryContainer />
+                </SimpleGrid>
               </Box>
               <Footer />
             </Flex>
