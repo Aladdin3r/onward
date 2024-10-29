132ce249d17c2a361d1d59de6c1409002635054f
holdeno0 <hbudiman1@my.bcit.ca>
2024-10-28 19:50:38 -0700

    fixing sizing

--- styles/components/ImprovementSteps.js
+++ styles/components/ImprovementSteps.js
@@ -5,7 +5,7 @@ export default function ImprovementSteps() {
   const steps = [
     {
       title: "Reduce Filler Words",
-      description: "Limit filler words for clearer, more confident responses.",
+      description: "Limit filler words for clearer responses.",
       alert: true,
     },
     {
@@ -25,19 +25,22 @@ export default function ImprovementSteps() {
       p="6"
       bg="white"
       boxShadow="md"
-      maxW="lrg"
-      minH="400px"
+      maxW="lg" // Adjusted to a valid value
+      minH="450px"
       mt=""
-      border="1px" 
-      borderColor="#E6EAF2" 
-      borderRadius="md" 
-      mr=''
+      border="1px"
+      borderColor="#E6EAF2"
+      borderRadius="24"
+      display="flex" // Added flex display
+      flexDirection="column" // Ensure content stacks vertically
+      alignItems="center" // Center items horizontally
+      justifyContent="center" // Center items vertically if necessary
     >
-      <Text fontSize="20px" fontWeight="semibold" color="#4B00B5" mb="6">
+      <Text fontSize="20px" fontWeight="semibold" color="#4B00B5" mb="6" textAlign="center">
         Steps to Improve Your Interview
       </Text>
 
-      <VStack align="start" spacing="10">
+      <VStack align="left" spacing="10"> {/* Center aligned */}
         {steps.map((step, index) => (
           <HStack key={index} align="start" spacing="3">
             <Image
@@ -48,7 +51,7 @@ export default function ImprovementSteps() {
             />
 
             <Box>
-              <Text fontWeight="semibold" color="gray.800">
+              <Text fontWeight="semibold" color="gray.800" textAlign="left"> {/* Change to left for individual text alignment */}
                 {step.title}
                 {/* {step.alert && (
                   <Text as="span" color="red.500" ml="1">
@@ -56,7 +59,7 @@ export default function ImprovementSteps() {
                   </Text>
                 )} */}
               </Text>
-              <Text color="gray.600" fontSize="16px" fontWeight="light">
+              <Text color="gray.600" fontSize="16px" fontWeight="light" textAlign="left">
                 {step.description}
               </Text>
             </Box>
@@ -65,7 +68,7 @@ export default function ImprovementSteps() {
       </VStack>
 
       <Button
-        mt="6"
+        mt="8"
         bg="#EA4A7D"
         color='#FFFFFF'
         width="100%"
