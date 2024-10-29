151c5f39e07c14d5761f42c14c7e156610cb9d7c
jacksonnsaini <jsaini34@my.bcit.ca>
2024-10-28 22:02:29 -0700

    adding pdf card

---
+++ styles/components/PDFCard.js
@@ -0,0 +1,11 @@
+import { Box, Image, Text } from '@chakra-ui/react'
+
+export default function PDFCard({ title, size}) {
+    return (
+        <Box p={3} w="600px" h="65px" bg="gray.50" borderRadius="md" borderWidth="1px" display="flex" alignItems="center">
+                  <Image src="/images/PDF-icon.svg" alt="PDF Icon" width="32px" style={{ marginRight: '8px' }} />
+                    <Text fontSize="xs" flex="1">{title}</Text>
+                    <Text fontSize="xxs" color="gray.500">{size}</Text>
+                  </Box>
+    )
+ }
